17039cc1d40777f1320ecb9526e155ba84df779d
jake-den <116654384+jake-den@users.noreply.github.com>
2022-10-25 22:54:22 +0900

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -19,11 +19,12 @@ task('accounts', 'Prints the list of accounts', async () => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: '0.8.6',
-  settings: {
-    optimizer: {
-      enabled: true,
-      runs: 200
-    }
-  }
-}
+  solidity: {
+    version: "0.8.8",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
